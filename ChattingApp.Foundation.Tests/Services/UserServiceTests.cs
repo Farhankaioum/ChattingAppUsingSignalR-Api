205f8358f1b3e91981b143ad11cf88b9940f3939
Farhan Kaioum <km1472233@gmail.com>
2021-01-16 16:29:04 +0600

    Foundation library test project added

---
+++ ChattingApp.Foundation.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace ChattingApp.Foundation.Tests.Services
+{
+    public class UserServiceTests
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void AddUser_UserProvided_RegisterUser()
+        {
+
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeClean()
+        {
+
+        }
+    }
+}
